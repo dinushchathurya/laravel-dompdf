884fe538cc8b7337fbb7f86d8b77daab07947783
dinushchathurya <dinushchathurya21@gmail.com>
2021-03-28 22:10:56 +0530

    feat: create footer component

---
+++ resources/views/layouts/inc/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="br-footer">
+    <div class="footer-left">
+        <div class="mg-b-2">Copyright &copy; 2021. CodingtrickS. All Rights Reserved.</div>
+        <div>Design and Developed by <a href="https://codingtricks.io">Codingtricks.io</a></div>
+    </div>
+    <div class="footer-right d-flex align-items-center">
+        <span class="tx-uppercase mg-r-10">Share:</span>
+        <a target="_blank" class="pd-x-5" href="https://facebook.com/dinush.chathurya"><i class="fa fa-facebook tx-20"></i></a>
+        <a target="_blank" class="pd-x-5" href="https://twitter.com/dinushchathurya"><i class="fa fa-twitter tx-20"></i></a>
+
+    </div>
+</footer>
